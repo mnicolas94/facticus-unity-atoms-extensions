b2083f3f4454029053ae252fb298ed0d73f0ea7e
mnicolas94 <miguelcok27@gmail.com>
2023-09-19 17:51:00 -0400

    feat: implement ObjectReferenceInitializer

---
+++ Runtime/ObjectReferenceInitializer.cs
@@ -0,0 +1,33 @@
+﻿using UnityAtoms;
+using UnityEngine;
+
+namespace UnityAtomsExtensions
+{
+    public class ObjectReferenceInitializer<T, TV, P, C, V, E1, E2, F, VI> : MonoBehaviour
+        where T : Object
+        where TV : AtomReference<T, P, C, V, E1, E2, F, VI>
+        where P : struct, IPair<T>
+        where C : AtomBaseVariable<T>
+        where V : AtomVariable<T, P, E1, E2, F>
+        where E1 : AtomEvent<T>
+        where E2 : AtomEvent<P>
+        where F : AtomFunction<T, T>
+        where VI : AtomVariableInstancer<V, P, T, E1, E2, F>
+    {
+        [SerializeField] private T _value;
+        [SerializeField] private TV _variable;
+
+        private void OnEnable()
+        {
+            _variable.Value = _value;
+        }
+
+        private void OnDisable()
+        {
+            if (_variable.Value == _value)
+            {
+                _variable.Value = null;
+            }
+        }
+    }
+}
