6ad21783dea28498077d589cbb831bc5f3282c43
mnicolas94 <miguelcok27@gmail.com>
2023-09-21 16:13:02 -0400

    feat: implement ActionUnityEvent.cs

---
+++ Runtime/ActionUnityEvent.cs
@@ -0,0 +1,17 @@
+﻿using UnityAtoms;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace UnityAtomsExtensions
+{
+    [CreateAssetMenu(menuName = "Unity Atoms/Actions/Unity event", fileName = "Action")]
+    public class ActionUnityEvent : AtomAction
+    {
+        [SerializeField] private UnityEvent _actions;
+
+        public override void Do()
+        {
+            _actions.Invoke();
+        }
+    }
+}
