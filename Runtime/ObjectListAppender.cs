b098e8a2e7d71187456ced8e216376ff68b8659c
mnicolas94 <miguelcok27@gmail.com>
2023-09-22 12:26:40 -0400

    feat: implement ObjectListAppender

---
+++ Runtime/ObjectListAppender.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityAtoms;
+using UnityEngine;
+
+namespace UnityAtomsExtensions
+{
+    public class ObjectListAppender<T, TL, TE> : MonoBehaviour
+        where T : Object
+        where TL : AtomValueList<T, TE>
+        where TE : AtomEvent<T>
+    {
+        [SerializeField] private T _element;
+        [SerializeField] private RemoveMode _removeOn;
+        [SerializeField] private List<TL> _listsToAppend;
+
+        private void OnEnable()
+        {
+            AddToLists();
+        }
+
+        private void OnDisable()
+        {
+            if (_removeOn == RemoveMode.OnDisable)
+            {
+                RemoveFromLists();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (_removeOn == RemoveMode.OnDestroy)
+            {
+                RemoveFromLists();
+            }
+        }
+
+        private void AddToLists()
+        {
+            foreach (var list in _listsToAppend)
+            {
+                list.Add(_element);
+            }
+        }
+        
+        private void RemoveFromLists()
+        {
+            foreach (var list in _listsToAppend)
+            {
+                list.Remove(_element);
+            }
+        }
+    }
+}
