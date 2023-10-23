9399fd9e04b048cc5c81b48cf433292f9c8e0377
mnicolas94 <miguelcok27@gmail.com>
2023-10-23 13:07:55 -0400

    feat: add _target field to GameObjectVariableInitializer class

--- Runtime/GameObjectVariableInitializer.cs
+++ Runtime/GameObjectVariableInitializer.cs
@@ -6,20 +6,31 @@ namespace UnityAtomsExtensions
 {
     public class GameObjectVariableInitializer : MonoBehaviour
     {
+        [SerializeField] private GameObject _target;
         [SerializeField] private GameObjectVariable _variable;
         [SerializeField] private bool _setNullOnDisable = true;
 
         private void OnEnable()
         {
-            _variable.Value = gameObject;
+            _variable.Value = _target;
         }
 
         private void OnDisable()
         {
-            if (_setNullOnDisable && _variable.Value == gameObject)
+            if (_setNullOnDisable && _variable.Value == _target)
             {
                 _variable.Value = null;
             }
         }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (_target == null)
+            {
+                _target = gameObject;
+            }
+        }
+#endif
     }
 }
