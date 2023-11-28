abdb29a762d0934da5f67c81f9ff91ecb0e6973c
mnicolas94 <miguelcok27@gmail.com>
2023-11-28 08:05:21 -0500

    feat: implement AtomEventInstancerExtensions.WaitInstantiation method

---
+++ Runtime/Extensions/AtomEventInstancerExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using UnityAtoms;
+
+namespace UnityAtomsExtensions.Extensions
+{
+    public static class AtomEventInstancerExtensions
+    {
+        /// <summary>
+        /// Asynchronous method to wait for an Event instancer to instantiate the event. Useful if you need to
+        /// access the event inside an Awake or OnEnable method where the instancer's event may not be instantiated yet.
+        /// </summary>
+        /// <param name="instancer"></param>
+        /// <param name="ct"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="E"></typeparam>
+        public static async Task WaitInstantiation<T, E>(this AtomEventInstancer<T, E> instancer, CancellationToken ct)
+            where E : AtomEvent<T>
+        {
+            while (instancer.Event == null && !ct.IsCancellationRequested)
+            {
+                await Task.Yield();
+            }
+        }
+    }
+}
